455e9f7c84e25da4834c9f16006e80f3621098a9
js <tlssk00@gmail.com>
2022-01-24 16:55:00 +0900

    열라면 업로드

---
+++ js/yeulramen.js
@@ -0,0 +1,51 @@
+window.onload = function () {
+
+    var swiper = new Swiper('.slide1', {
+        observer: true,
+        observeParents: true,
+        keyboard: {
+            enabled: true,
+        },
+    });
+
+    var swiper = new Swiper('.slide2', {
+        observer: true,
+        observeParents: true,
+        keyboard: {
+            enabled: true,
+        },
+    });
+
+    var swiper = new Swiper('.slide3', {
+        observer: true,
+        observeParents: true,
+        keyboard: {
+            enabled: true,
+        },
+    });
+
+    const slide1 = document.querySelector(".article2_inner > div:nth-child(2)");
+    console.log(slide1);
+    const slide2 = document.querySelector(".article2_inner > div:nth-child(3)");
+    console.log(slide2);
+    const slideBt1 = document.querySelector(".article2_inner > div:nth-child(1) > button:nth-child(1)");
+    console.log(slideBt1);
+    const slideBt2 = document.querySelector(".article2_inner > div:nth-child(1) > button:nth-child(2)");
+    console.log(slideBt2);
+    var slideBt1On = function(){
+        slide1.classList.add("on");
+        slide2.classList.remove("on");
+        slideBt1.classList.add("on");
+        slideBt2.classList.remove("on");
+    }
+    console.log(slideBt1On);
+    var slideBt2On = function(){
+        slide1.classList.remove("on");
+        slide2.classList.add("on");
+        slideBt1.classList.remove("on");
+        slideBt2.classList.add("on");
+    }
+    console.log(slideBt2On);
+    slideBt1.addEventListener('click', slideBt1On);
+    slideBt2.addEventListener('click', slideBt2On);
+}
