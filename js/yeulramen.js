e7238c1b911478b2c17e05ffe0a3037ac5289e2e
Your Name <you@example.com>
2022-01-26 12:18:22 +0900

    zjzj

--- js/yeulramen.js
+++ js/yeulramen.js
@@ -36,36 +36,36 @@ window.onload = function () {
 
     // 슬라이드1,2 클릭 이벤트
     const slide1 = document.querySelector(".article2_inner > div:nth-child(2)");
-    console.log(slide1);
+    // console.log(slide1);
     const slide2 = document.querySelector(".article2_inner > div:nth-child(3)");
-    console.log(slide2);
+    // console.log(slide2);
     const slideBt1 = document.querySelector(".article2_inner > div:nth-child(1) > button:nth-child(1)");
-    console.log(slideBt1);
+    // console.log(slideBt1);
     const slideBt2 = document.querySelector(".article2_inner > div:nth-child(1) > button:nth-child(2)");
-    console.log(slideBt2);
+    // console.log(slideBt2);
     var slideBt1On = function(){
         slide1.classList.add("on");
         slide2.classList.remove("on");
         slideBt1.classList.add("on");
         slideBt2.classList.remove("on");
     }
-    console.log(slideBt1On);
+    // console.log(slideBt1On);
     var slideBt2On = function(){
         slide1.classList.remove("on");
         slide2.classList.add("on");
         slideBt1.classList.remove("on");
         slideBt2.classList.add("on");
     }
-    console.log(slideBt2On);
+    // console.log(slideBt2On);
     slideBt1.addEventListener('click', slideBt1On);
     slideBt2.addEventListener('click', slideBt2On);
 
     const moveBt1 = document.querySelector(".header_inner > div > button:nth-child(1)");
-    console.log(moveBt1);
+    // console.log(moveBt1);
     const moveBt2 = document.querySelector(".header_inner > div > button:nth-child(2)");
-    console.log(moveBt2);
+    // console.log(moveBt2);
     const body = document.querySelector("body");
-    console.log(body);
+    // console.log(body);
     const article2 = document.querySelector(".article2")
     let move = function(a){
         window.scroll({
