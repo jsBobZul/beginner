4a0c0a17f68340b26d6d573d17726682fbd63850
unknown <you@example.com>
2022-01-28 15:18:07 +0900

    !!!

--- js/yeulramen.js
+++ js/yeulramen.js
@@ -24,21 +24,14 @@ window.onload = function () {
             enabled: true,
         },
         pagination: '.swiper-pagination',
-        effect: 'coverflow',
         grabCursor: true,
         centeredSlides: true,
-        slidesPerView: '3',
+        slidesPerView: 3,
+        spaceBetween:30,
         breakpoints: {
-            1199: {
+            767: {
                 slidesPerView: 1,
             },
-        },
-        coverflow: {
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows : true
         },
     });
 
