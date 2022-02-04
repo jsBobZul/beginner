c550f732c9515697303e8bf760d2e9cd1d13e167
Your Name <you@example.com>
2022-02-04 17:33:29 +0900

    Update kimsubpage1.js

--- js/kimsubpage1.js
+++ js/kimsubpage1.js
@@ -26,6 +26,7 @@ window.onload = function () {
   const article5 = document.querySelector(".article5");
   const article2Top = article2.offsetTop-70;
   const article4Top = article4.offsetTop-70;
+  const article5Top = article5.offsetTop-70;
   sub1Bt[0].addEventListener("click", function(){
     move(article2Top);
   });
@@ -33,8 +34,22 @@ window.onload = function () {
     move(article4Top);
   });
   sub1Bt[2].addEventListener("click", function(){
-    move(article5.offsetTop);
+    move(article5Top);
   });
+
+  // const hashAritcle = [article2, article4, article5];
+  // const hashAritcle = [article2Top, article4Top, article5Top];
+
+  // hashAritcle.forEach(function(eachArticle, index){
+  //     let abcd = eachArticle.offsetTop-70;
+  //     console.log(eachArticle);
+  //   });
+    // sub1Bt.forEach(function(eachBt){
+    //   console.log(eachBt);
+    //   eachBt.addEventListener("click", function(){
+    //     move(hashAritcle);
+    //   });
+    // });
 
   // 타 페이지에서 이동
   if(location.hash === '#a1'){
@@ -46,6 +61,11 @@ window.onload = function () {
     window.scroll({
       left: 0,
       top: article4Top
+    });
+  }else if(location.hash === "#a3"){
+    window.scroll({
+      left: 0,
+      top: article5Top
     });
   };
 
