58efb5be31927b0e6a05cc36874c70a0dce117f8
Your Name <you@example.com>
2022-02-11 13:04:58 +0900

    !!

--- js/chungjukim.js
+++ js/chungjukim.js
@@ -175,4 +175,33 @@ window.onload = function () {
       };
   });
 
+  // ------- 로그인 유효성 검사 ----------
+
+  // 대/소문자, 숫자
+  let idReg = RegExp(/^[a-zA-Z0-9]/);
+  // 최소 8 자, 대문자 하나 이상, 소문자 하나, 숫자 하나 및 특수 문자 하나 이상
+  let passwordReg = RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{5,}/);
+  const kimId = document.querySelector('input[placeholder^="아이디"]');
+  const kimPw = document.querySelector('input[placeholder^="비밀번호"]');
+  const idLog = document.querySelector('.id_log');
+  const pwLog = document.querySelector('.pass_log');
+
+  // ----------아이디 이벤트--------
+  kimId.addEventListener("focus", function(){
+    let idText = document.createTextNode("대/소문자 구분 없이 영어, 숫자만 가능합니다.");
+    idLog.appendChild(idText);
+  });
+  kimId.addEventListener("blur", function(){
+    idLog.innerText = "";
+  });
+
+  // -----------패스워드 이벤트-------------
+  kimPw.addEventListener("focus", function(){
+    let pwText = document.createTextNode("대/소문자,숫자,특수문자 포함 최소 5자 이상 비밀번호 입력해주세요.");
+    pwLog.appendChild(pwText);
+  });
+  kimPw.addEventListener("blur", function(){
+    pwLog.innerText = "";
+  });
+
 };
