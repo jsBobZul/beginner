33339670448a8c458be5b3f51826fc4b833e3d0e
Your Name <you@example.com>
2022-02-11 14:57:24 +0900

    Update chungjukim.js

--- js/chungjukim.js
+++ js/chungjukim.js
@@ -178,9 +178,10 @@ window.onload = function () {
   // ------- 로그인 유효성 검사 ----------
 
   // 대/소문자, 숫자
-  let idReg = RegExp(/^[a-zA-Z0-9]/);
-  // 최소 8 자, 대문자 하나 이상, 소문자 하나, 숫자 하나 및 특수 문자 하나 이상
-  let passwordReg = RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{5,}/);
+  let idReg = RegExp(/^[a-zA-Z0-9]+$/);
+  // 최소 5자, 대문자 하나 이상, 소문자 하나, 숫자 하나 및 특수 문자 하나 이상
+  let passwordReg = RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{5,}$/);
+
   const kimId = document.querySelector('input[placeholder^="아이디"]');
   const kimPw = document.querySelector('input[placeholder^="비밀번호"]');
   const idLog = document.querySelector('.id_log');
@@ -191,17 +192,57 @@ window.onload = function () {
     let idText = document.createTextNode("대/소문자 구분 없이 영어, 숫자만 가능합니다.");
     idLog.appendChild(idText);
   });
-  kimId.addEventListener("blur", function(){
-    idLog.innerText = "";
-  });
+
+  // kimId.addEventListener("blur", function(){
+  //   idLog.innerText = "";
+  //   idLog.style.color = "#c4c4c4";
+  //   if(kimId.value !== ""){
+  //     if(!idReg.test(kimId.value)){
+  //         idLog.style.color = "red";
+  //         idLog.innerText = "정확한 값을 입력해주세요.";
+  //         idLog.appendChild(document.createElement("br"));
+  //         kimId.focus();
+  //     };
+  //   };
+  // });
+  // --------
+
 
   // -----------패스워드 이벤트-------------
   kimPw.addEventListener("focus", function(){
-    let pwText = document.createTextNode("대/소문자,숫자,특수문자 포함 최소 5자 이상 비밀번호 입력해주세요.");
+    let pwText = document.createTextNode("대/소문자,숫자,특수문자 포함한 최소 5자 이상 비밀번호 입력해주세요.");
     pwLog.appendChild(pwText);
   });
-  kimPw.addEventListener("blur", function(){
-    pwLog.innerText = "";
-  });
+
+
+  let logInFocus = function logInFocus(el, el2, el3){
+    el.innerText = "";
+    el.style.color = "#c4c4c4";
+    if(el2.value !== ""){
+      if(!el3.test(el2.value)){
+        el.style.color = "red";
+        el.innerText = "정확한 값을 입력해주세요.";
+        el.appendChild(document.createElement("br"));
+        el2.focus();
+      };
+    };
+  };
+
+  kimId.addEventListener("blur", logInFocus(idLog, kimId, idReg));
+  kimPw.addEventListener("blur", logInFocus(pwLog, kimPw, passwordReg));
+
+
+  // kimPw.addEventListener("blur", function(){
+  //   pwLog.innerText = "";
+  //   pwLog.style.color = "#c4c4c4";
+  //   if(kimPw.value !== ""){
+  //     if(!passwordReg.test(kimPw.value)){
+  //       pwLog.style.color = "red";
+  //         pwLog.innerText = "정확한 값을 입력해주세요.";
+  //         pwLog.appendChild(document.createElement("br"));
+  //         kimPw.focus();
+  //     };
+  //   };
+  // });
 
 };
