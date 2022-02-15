c61f7d91e9221e394904cc6b54f063bbea684345
Your Name <you@example.com>
2022-02-15 17:10:02 +0900

    인덱스 골격 완성

--- js/index.js
+++ js/index.js
@@ -4,6 +4,18 @@ window.onload = function () {
     navBtn.addEventListener("click", function(){
         header.classList.toggle("on");
     });
+    
+    const mainBtn = document.querySelector(".slide_button");
+    const wrapping = document.querySelector(".wrapping");
+    let index = 1;
+    mainBtn.addEventListener("click", function(){
+        wrapping.style.left = `-${index*100}%`;
+        mainBtn.style.transform = `rotate(${index*120}deg)`;
+        index++;
+        if(index === 3){
+            index = 0;
+        };
 
+    });
 
 };
