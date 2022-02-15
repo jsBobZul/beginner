a330b0bbbd64dc27ecde7b5d2106e92b926d5db8
jsBobZul <tlssk00@gmail.com>
2022-02-15 23:25:46 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 window.onload = function () {
     const mainInner = document.querySelector(".main_inner");
     const navBtn = document.querySelector(".nav_button");
+
+    // grobal Nav 이벤트
     navBtn.addEventListener("click", function(){
         mainInner.classList.toggle("on");
     });
@@ -8,6 +10,8 @@ window.onload = function () {
     const mainBtn = document.querySelector(".slide_button");
     const mainBtnInner = document.querySelector(".slide_button > span");
     const wrapping = document.querySelector(".wrapping");
+
+    // 메인 슬라이드 구현
     let index = 1;
     let slideEvent = function (){
         wrapping.style.left = `-${index*100}%`;
@@ -17,10 +21,10 @@ window.onload = function () {
             index = 0;
         };
     };
-    mainBtn.addEventListener("click", slideEvent);
-    wrapping.addEventListener("mouseup", slideEvent);
+    mainBtn.addEventListener("click", slideEvent); // 메인 버튼 클릭 시 슬라이드 동작
+    wrapping.addEventListener("mouseup", slideEvent); // 슬라이드 마우스 이벤트
 
-    let globalBtn = document.querySelectorAll('header > button');
+    let globalBtn = document.querySelectorAll('header > button'); // 글로벌 네비게이션 슬라이드 동기화
     for(let i = 0; i <  globalBtn.length; i++){
         globalBtn[i].addEventListener('click', function(){
             wrapping.style.left = `-${i*100}%`;
