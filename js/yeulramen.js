ca96a98783d04e1c3bc9154ec7e7b06bdde705e1
jsBobZul <tlssk00@gmail.com>
2022-03-02 09:32:05 +0900

    열라면 콘솔로그 삭제

--- js/yeulramen.js
+++ js/yeulramen.js
@@ -74,20 +74,15 @@ window.onload = function () {
 
         // 슬라이드1,2 클릭 이벤트
         var slide1 = document.querySelector(".article2_inner > div:nth-child(2)");
-        // console.log(slide1);
         var slide2 = document.querySelector(".article2_inner > div:nth-child(3)");
-        // console.log(slide2);
         var slideBt1 = document.querySelector(".article2_inner > div:nth-child(1) > button:nth-child(1)");
-        // console.log(slideBt1);
         var slideBt2 = document.querySelector(".article2_inner > div:nth-child(1) > button:nth-child(2)");
-        // console.log(slideBt2);
         var slideBt1On = function () {
             slide1.classList.add("on");
             slide2.classList.remove("on");
             slideBt1.classList.add("on");
             slideBt2.classList.remove("on");
         };
-        // console.log(slideBt1On);
         var slideBt2On = function () {
             slide1.classList.remove("on");
             slide2.classList.add("on");
@@ -95,7 +90,6 @@ window.onload = function () {
             slideBt2.classList.add("on");
             swiper2();
         };
-        // console.log(slideBt2On);
         slideBt1.addEventListener('click', slideBt1On);
         slideBt2.addEventListener('click', slideBt2On);
 
