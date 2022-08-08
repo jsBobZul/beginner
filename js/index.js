8cf133e5feed6ffce062ad0ef2c27a778e47ab6a
/ <>
2022-08-08 13:04:07 +0900

    loop 추가
    loop기능 구현만 했음

--- js/index.js
+++ js/index.js
@@ -6,7 +6,8 @@ window.onload = function () {
     const mainBtn = document.querySelector(".slide_button");
     const mainBtnInner = document.querySelector(".slide_button span");
     const wrapping = document.querySelector(".wrapping");
-    const project = document.querySelector(".project");
+    const firstProject = wrapping.firstElementChild;
+    const lastProject = wrapping.lastElementChild; //loop 시 사용
     let projects = document.querySelectorAll('.project');
     let globalBtn = document.querySelectorAll('header button');
     
@@ -18,37 +19,82 @@ window.onload = function () {
     });
 
     // 메인 슬라이드 구현
-    let index = 0;
-    let rotate = 360/projects.length;
-    const x = 360/projects.length
-    let numReg = RegExp(/[^0-9]/g);
+    let index = 1; //loop 시 1, 아닐 시 0
+    let rotate = 360/(projects.length);
+    const x = 360/(projects.length);
+    // let numReg = RegExp(/[^0-9]/g);
     wrapping.ondragstart = function () { //다른 요소 드래그 방지
         return false;
     };
 
-    let slideRightEvent = function () {
-        index++;
-        if (index === projects.length) {
-                index = projects.length-1;
+    const LOOPFLAG = true;
+    //=======================loop 시 사용========================
+    const cloenTime = 100;
+    let firstClone = firstProject.cloneNode(true);
+    let lastClone = lastProject.cloneNode(true);
+    wrapping.appendChild(firstClone);
+    wrapping.insertBefore(lastClone, firstProject);
+    projects = document.querySelectorAll('.project');
+
+    wrapping.style.width = `${projects.length}00%`;
+    wrapping.style.left = `-100%`;
+    wrapping.style.transition = `none`;
+    for (let i = 0; i < projects.length; i++) {
+        projects[i].style.width = `${100 / projects.length}%`;
+      };
+
+      let slideRightEvent = ()=>{
+          rotate = index * x;
+          mainBtnInner.style.transform = `rotate(${rotate}deg)`;
+          index++;
+          wrapping.style.left = `-${index}00%`;
+          if(index > projects.length-2){
+              setTimeout(()=>{
+                  wrapping.style.left = `-100%`;
+                  index = 1;
+                }, cloenTime);
             };
-        wrapping.style.left = `-${index*100}%`;
-        mainBtnInner.style.transform = `rotate(${index*x}deg)`;
-        rotate = index * x;
         };
-    
-    let slideLeftEvent = function () {
-        let wrappingLeft = window.getComputedStyle(wrapping).left;
-        let projectWidth = window.getComputedStyle(project).width;
-        let wLeft = wrappingLeft.replace(numReg, ""); //숫자 변환
-        let pWidth = projectWidth.replace(numReg, ""); //숫자 변환
-        let responeSlide = (wLeft / pWidth - 1) * 100;
-        if (responeSlide >= 0) {
+        
+        let slideLeftEvent = () => {
             rotate = rotate - x;
-            wrapping.style.left = `${-responeSlide}%`;
-            mainBtnInner.style.transform = `rotate(${rotate}deg)`;
             index--;
-        };
+            wrapping.style.left = `-${index}00%`;
+            if(index == 0){
+                setTimeout(() =>{
+                    wrapping.style.transition = `none`;
+                    wrapping.style.left = `-${(projects.length-2)}00%`;
+                    index = projects.length - 2;
+                }, cloenTime)
+            };
+            mainBtnInner.style.transform = `rotate(${rotate}deg)`;
     };
+    //=======================/loop 시 사용===========================
+    //=======================loop 해제 시 사용========================
+    // let slideRightEvent = function () {
+        //     index++;
+        //     if (index === projects.length) {
+    //             index = projects.length-1;
+    //         };
+    //     wrapping.style.left = `-${index*100}%`;
+    //     mainBtnInner.style.transform = `rotate(${index*x}deg)`;
+    //     rotate = index * x;
+    //     };
+    
+    // let slideLeftEvent = function () {
+        //     let wrappingLeft = window.getComputedStyle(wrapping).left;
+        //     let projectWidth = window.getComputedStyle(firstProject).width;
+        //     let wLeft = wrappingLeft.replace(numReg, ""); //숫자 변환
+        //     let pWidth = projectWidth.replace(numReg, ""); //숫자 변환
+        //     let responeSlide = (wLeft / pWidth - 1) * 100;
+        //     if (responeSlide >= 0) {
+            //         rotate = rotate - x;
+            //         wrapping.style.left = `${-responeSlide}%`;
+            //         mainBtnInner.style.transform = `rotate(${rotate}deg)`;
+            //         index--;
+            //     };
+        //};
+    //=======================/loop 해제 시 사용========================
     mainBtn.addEventListener("click", slideRightEvent); // 메인 버튼 클릭 시 슬라이드 동작
     mainBtn.addEventListener("contextmenu", function (event) {
         event.preventDefault();
